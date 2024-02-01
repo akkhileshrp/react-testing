232f4987fbbf8fabad5519cc948b204cf4503e6d
AKKHILESHRP <akkhileshrp2003@gmail.com>
2024-02-01 23:23:09 +0530

    localstorage initailized

--- src/UsersData.jsx
+++ src/UsersData.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Users = ({ userData }) => {
+  const [state, setState] = useState(userData);
+
+  useEffect(() => {
+    const storage = localStorage.getItem("data");
+    if (storage) {
+      const parsedData = JSON.parse(storage);
+      if (Array.isArray(parsedData) && parsedData.length > 0) {
+        setState(parsedData);
+      }
+    }
+  }, [state]);
+
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(state));
+  }, [state]);
+
   return (
     <div>
       <h2>User Data</h2>
       <ul>
-        {userData.map((user, index) => {
-          return (
-            <li key={index}>
-              Name: {user.name}, Email: {user.email}
-            </li>
-          )
-        }
-        )}
+        {state.map((user, index) => (
+          <li key={index}>
+            Name: {user.name}, Email: {user.email}
+          </li>
+        ))}
       </ul>
     </div>
   );
